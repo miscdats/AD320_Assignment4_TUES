17803d44e7a20bbbf7bd729daae14acbd3863849
Bob McHenry <bobmchenryvideo@gmail.com>
2019-02-01 02:17:16 -0800

    Sample div element added

---
+++ bob-mchenry.php
@@ -0,0 +1,10 @@
+<?php 
+	echo 
+	"<div style='border: 1px solid black;"
+	. "margin:0 auto;"
+	. "padding:10px;" 
+	. "display: inline-block'>"
+	. "<h3>Hello World</h3>"
+	. "<ul> <li>List item 1</li><li>List Item 2</li></ul>"
+	. "</div>";
+?>
