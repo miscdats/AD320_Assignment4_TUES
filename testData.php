672c505515b3fb877b027dfc7c4d98448bad188a
Bob McHenry <BobMcHenry@users.noreply.github.com>
2019-02-01 01:15:56 -0800

    Create testData.php

---
+++ testData.php
@@ -0,0 +1,13 @@
+<?php
+
+$dataToPrintToScreen = [
+    'Fruits' => ['apple', 'raspberry', 'pear', 'banana'],
+    'Vegetables' => ['peas', 'carrots', 'cabbage'],
+    'Grains' => ['wheat', 'rice', 'oats'],
+    'Cars' => ['Volvo', 'Audi', 'Volkswagen'],
+    'Cities' => ['Cancun', 'Madrid', 'Edinburgh', 'Los Angeles'],
+    'Beverages' => ['soda', 'pop', 'beer', 'wine', 'water'],
+    'Programming Languages' => ['COBOL', 'C++', 'FORTRAN', 'Perl', 'Arnold C']
+];
+
+?>
