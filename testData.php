ce32190c56695e573c299c58ae4bc6e71b8b9c99
Bob McHenry <BobMcHenry@users.noreply.github.com>
2019-02-01 02:00:22 -0800

    Update testData.php

--- testData.php
+++ testData.php
@@ -1,13 +1,17 @@
 <?php
 
-$dataToPrintToScreen = [
-    'Fruits' => ['apple', 'raspberry', 'pear', 'banana'],
-    'Vegetables' => ['peas', 'carrots', 'cabbage'],
-    'Grains' => ['wheat', 'rice', 'oats'],
-    'Cars' => ['Volvo', 'Audi', 'Volkswagen'],
-    'Cities' => ['Cancun', 'Madrid', 'Edinburgh', 'Los Angeles'],
-    'Beverages' => ['soda', 'pop', 'beer', 'wine', 'water'],
-    'Programming Languages' => ['COBOL', 'C++', 'FORTRAN', 'Perl', 'Arnold C']
-];
+   function get2dArray() {
+        $dataToPrintToScreen = [
+            'Fruits' => ['apple', 'raspberry', 'pear', 'banana'],
+            'Vegetables' => ['peas', 'carrots', 'cabbage'],
+            'Grains' => ['wheat', 'rice', 'oats'],
+            'Cars' => ['Volvo', 'Audi', 'Volkswagen'],
+            'Cities' => ['Cancun', 'Madrid', 'Edinburgh', 'Los Angeles'],
+            'Beverages' => ['soda', 'pop', 'beer', 'wine', 'water'],
+            'Programming Languages' => ['COBOL', 'C++', 'FORTRAN', 'Perl', 'Arnold C']
+        ];
+
+        return $dataToPrintToScreen;
+    }
 
 ?>
