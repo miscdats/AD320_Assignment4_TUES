2636bcc90e20f7659dae35ecf6a5865f959b41d9
Bob McHenry <BobMcHenry@users.noreply.github.com>
2019-02-04 22:23:29 -0800

    Update Maria-Kalusz.php

--- Maria-Kalusz.php
+++ Maria-Kalusz.php
@@ -1,12 +1,3 @@
 <?php
-require 'testData.php';
-
-foreach(get2dArray() as $key => $value) {
-    echo "<h3>" . $key . "</h3>";
-    echo "<ul>";
-    foreach($value as $list) {
-        echo "<div><li>" . $list . "</li></div>";
-    }
-    echo "</ul>";
-}
+ echo "Solution Hidden.";
 ?>
