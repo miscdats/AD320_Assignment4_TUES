7e3d547999a5c4c4f60d34636f198cb1bcc772e7
gillilands19 <gillilands19@gmail.com>
2019-02-08 20:09:54 -0800

    loops and echoes data from get2dArray function, adds styles.css

---
+++ sean-gilliland.php
@@ -0,0 +1,37 @@
+<?php
+
+require('testData.php');
+
+$data = get2dArray();
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    
+<meta charset="utf-8" />
+<meta name="robots" content="noindex,nofollow" />
+<meta name="viewport" content="width=device-width" />
+<meta name="description" content="Assignment 4" />
+<link rel="stylesheet" href="style.css">
+    
+<title>Sean G. Assignment 4</title>
+</head>
+
+<body>
+
+  <div class="site-wrapper">
+    <div class="content-wrapper">
+    <?php foreach($data as $key => $value): ?>
+      <div class="data-container"><h3><?php echo $key ?></h3>
+      <ul>
+      <?php foreach($value as $val): ?>
+      <li><?php echo $val ?></li>
+      <?php endforeach; ?>
+      </ul>
+      </div>
+    <?php endforeach; ?>
+    </div>
+  </div>
+</body>
